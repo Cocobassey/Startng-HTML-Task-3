46bd766e5e3a070f40d74c4023e19df7935df17d
Henry CocoBassey <58895990+Cocobassey@users.noreply.github.com>
2020-04-19 21:20:38 +0100

    Add files via upload

---
+++ JS Task3.js
@@ -0,0 +1,33 @@
+let Array = [];
+function wholeNum(num) {
+    for (let i = 1; i <= num; i++) {
+        if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
+            Array.push('yu-gi-oh');
+        } 
+        else if (i % 3 === 0 && i % 5 === 0) {
+            Array.push('gi-oh');
+        } 
+        else if (i % 2 === 0 && i % 5 === 0) {
+            Array.push('yu-oh');
+        } 
+        else if (i % 2 === 0 && i % 3 === 0) {
+            Array.push('yu-gi');
+        }
+        else if (i % 5 === 0) {
+            Array.push('oh');
+        }
+        else if (i % 3 === 0) {
+            Array.push('gi');
+        }
+        else if (i % 2 === 0) {
+            Array.push('yu')
+        }
+        else {
+            Array.push(i)
+        }
+    }
+    console.log(Array);
+    return Array;
+}
+
+wholeNum(50);
